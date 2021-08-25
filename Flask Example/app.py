798abf58be66cb83481201f1416749ad017468b0
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2021-08-25 17:44:41 +0800

    Import flask example from class

---
+++ Flask Example/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, Response, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+data_url = 'data/food_consumption.csv'
+
+# DATA ENDPOINTS
+@app.route('/countries')
+def get_countries():
+    ## TODO: Get the countries and return as a list or JSON to the JS file
+    df = pd.read_csv(data_url)
+    df.columns = ['country', 'category', 'consumption', 'co2']
+    countries = sorted(list(df.country.unique()))
+
+    country_dict = pd.DataFrame(list(zip(countries, countries)), columns=['value', 'label']).to_json(orient="records")
+    return Response(country_dict, mimetype="application/json")
+
+@app.route('/data')
+def get_data():
+    data = pd.read_csv(data_url)
+    data.columns = ['country', 'category', 'consumption', 'co2']
+
+    data_json = data.to_json(orient="records")
+    return Response(data_json, mimetype="application/json")
+
+@app.route('/data/<country>')
+def get_country_data(country):
+    ## TODO: Get the data filtered by the provided country in the argument
+    df = pd.read_csv(data_url)
+    df.columns = ['country', 'category', 'consumption', 'co2']
+
+    filtered_df = df[df['country'] == country].to_json(orient='records')
+    return Response(filtered_df, mimetype="application/json")
+
+
+# STATIC PAGES
+@app.route('/about')
+def about():
+    return app.send_static_file('about.html')
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Flask Example/static/main.js
@@ -0,0 +1,58 @@
+var width = 500;
+var height = 500;
+var padding = 60;
+
+var svg = d3.select("#chart").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+// var url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv";
+
+d3.json('/data').then(function (formatted_data) {
+  var categories = [...new Set(formatted_data.map(function (d) { return d.category; }))];
+
+  var colorScale = d3.scaleOrdinal(categories, d3.schemeSet3);
+
+  var xMax = d3.max(formatted_data, function (d) { return d.consumption; });
+
+  var yMax = d3.max(formatted_data, function (d) { return d.co2; });
+
+  var xScale = d3.scaleLinear([-10, xMax], [padding, width - padding]);
+
+  var yScale = d3.scaleLinear([-50, yMax], [height - padding, padding]);
+
+  var xAxis = d3.axisBottom(xScale);
+  var yAxis = d3.axisLeft(yScale);
+
+  svg.append("g")
+    .attr("transform", "translate(0, " + (height - padding) + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("transform", "translate(" + padding + ", 0)")
+    .call(yAxis);
+
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height - padding + 40)
+    .attr("text-anchor", "middle")
+    .text("Food Consumption (kg/person/year)")
+
+  svg.append("text")
+    .attr("x", -height / 2)
+    .attr("y", padding - 40)
+    .attr("transform", "rotate(-90)")
+    .attr("text-anchor", "middle")
+    .text("CO2 Emissions (Kg CO2/person/year)")
+
+  svg.selectAll("circle")
+    .data(formatted_data)
+    .join("circle")
+    .attr("cx", d => xScale(d.consumption))
+    .attr("cy", function (d) { return yScale(d.co2); })
+    .attr("r", 5)
+    .attr("opacity", 0.4)
+    .style("fill", function (d) { return colorScale(d.category); });
+
+  console.log(colorScale("Beef"))
+})
