18d4581bcd9ca112ce22c5b97db358ee52fc8c57
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2021-08-29 19:17:55 +0800

    added select region data

--- app.py
+++ app.py
@@ -3,40 +3,62 @@ import pandas as pd
 
 app = Flask(__name__)
 
-data_url = 'data/food_consumption.csv'
+data_url = 'data/Family Income and Expenditure.csv'
 
 # DATA ENDPOINTS
-
-
-@app.route('/countries')
-def get_countries():
-    # TODO: Get the countries and return as a list or JSON to the JS file
+@app.route('/regions')
+def get_regions():
     df = pd.read_csv(data_url)
-    df.columns = ['country', 'category', 'consumption', 'co2']
-    countries = sorted(list(df.country.unique()))
+    df = df.iloc[:,[1]]         # get only Region column
 
-    country_dict = pd.DataFrame(list(zip(countries, countries)), columns=[
+    # rename region values
+    df.replace({'Region' : {'I - Ilocos Region': 'Region I', 'II - Cagayan Valley': 'Region II', 
+                'III - Central Luzon' : 'Region III', 'IVA - CALABARZON': 'Region IVA',
+                'IVB - MIMAROPA':'Region IVB','V - Bicol Region':'Region V', 
+                'VI - Western Visayas':'Region VI','VII - Central Visayas': 'Region VII', 
+                'VIII - Eastern Visayas': 'Region VIII', 'IX - Zasmboanga Peninsula': 'Region IX',
+                'X - Northern Mindanao': 'Region X', 'XI - Davao Region': 'Region XI',
+                'XII - SOCCSKSARGEN': 'Region XII', 'Caraga': 'Region XIII', ' ARMM': 'ARMM'}}, inplace=True)
+
+    # sort renamed region values
+    regions = sorted(list(df.Region.unique()))
+
+    # store in a dictionary format
+    regions_dict = pd.DataFrame(list(zip(regions, regions)), columns=[
                                 'value', 'label']).to_json(orient="records")
-    return Response(country_dict, mimetype="application/json")
+    
+    return Response(regions_dict, mimetype="application/json")
 
 
-@app.route('/data')
-def get_data():
-    data = pd.read_csv(data_url)
-    data.columns = ['country', 'category', 'consumption', 'co2']
+# @app.route('/countries')
+# def get_countries():
+#     # TODO: Get the countries and return as a list or JSON to the JS file
+#     df = pd.read_csv(data_url)
+#     df.columns = ['country', 'category', 'consumption', 'co2']
+#     countries = sorted(list(df.country.unique()))
 
-    data_json = data.to_json(orient="records")
-    return Response(data_json, mimetype="application/json")
+#     country_dict = pd.DataFrame(list(zip(countries, countries)), columns=[
+#                                 'value', 'label']).to_json(orient="records")
+#     return Response(country_dict, mimetype="application/json")
 
 
-@app.route('/data/<country>')
-def get_country_data(country):
-    # TODO: Get the data filtered by the provided country in the argument
-    df = pd.read_csv(data_url)
-    df.columns = ['country', 'category', 'consumption', 'co2']
+# @app.route('/data')
+# def get_data():
+#     data = pd.read_csv(data_url)
+#     data.columns = ['country', 'category', 'consumption', 'co2']
 
-    filtered_df = df[df['country'] == country].to_json(orient='records')
-    return Response(filtered_df, mimetype="application/json")
+#     data_json = data.to_json(orient="records")
+#     return Response(data_json, mimetype="application/json")
+
+
+# @app.route('/data/<country>')
+# def get_country_data(country):
+#     # TODO: Get the data filtered by the provided country in the argument
+#     df = pd.read_csv(data_url)
+#     df.columns = ['country', 'category', 'consumption', 'co2']
+
+#     filtered_df = df[df['country'] == country].to_json(orient='records')
+#     return Response(filtered_df, mimetype="application/json")
 
 
 # STATIC PAGES
--- static/js/main.js
+++ static/js/main.js
@@ -1,58 +1,72 @@
-var width = 500;
-var height = 500;
-var padding = 60;
+$(document).ready(function(){
+  var region_select = $('ul li #region_select')
 
-var svg = d3.select("#chart").append("svg")
-  .attr("width", width)
-  .attr("height", height);
+  d3.json('/regions').then(function(data){
+    data.forEach(function(elem){
+      region_select.append('<option value="'+ elem.value + '">'+ elem.label + '</option>');
+    });
+  });
 
-// var url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv";
 
-d3.json('/data').then(function (formatted_data) {
-  var categories = [...new Set(formatted_data.map(function (d) { return d.category; }))];
+  // example of miss unisse during class for food consumption guide
+  // var width = 500;
+  // var height = 500;
+  // var padding = 60;
 
-  var colorScale = d3.scaleOrdinal(categories, d3.schemeSet3);
+  // var svg = d3.select("#chart").append("svg")
+  //   .attr("width", width)
+  //   .attr("height", height);
 
-  var xMax = d3.max(formatted_data, function (d) { return d.consumption; });
+  // var url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-18/food_consumption.csv";
 
-  var yMax = d3.max(formatted_data, function (d) { return d.co2; });
+  // d3.json('/data').then(function (formatted_data) {
+  //   var categories = [...new Set(formatted_data.map(function (d) { return d.category; }))];
 
-  var xScale = d3.scaleLinear([-10, xMax], [padding, width - padding]);
+  //   var colorScale = d3.scaleOrdinal(categories, d3.schemeSet3);
 
-  var yScale = d3.scaleLinear([-50, yMax], [height - padding, padding]);
+  //   var xMax = d3.max(formatted_data, function (d) { return d.consumption; });
 
-  var xAxis = d3.axisBottom(xScale);
-  var yAxis = d3.axisLeft(yScale);
+  //   var yMax = d3.max(formatted_data, function (d) { return d.co2; });
 
-  svg.append("g")
-    .attr("transform", "translate(0, " + (height - padding) + ")")
-    .call(xAxis);
+  //   var xScale = d3.scaleLinear([-10, xMax], [padding, width - padding]);
 
-  svg.append("g")
-    .attr("transform", "translate(" + padding + ", 0)")
-    .call(yAxis);
+  //   var yScale = d3.scaleLinear([-50, yMax], [height - padding, padding]);
 
-  svg.append("text")
-    .attr("x", width / 2)
-    .attr("y", height - padding + 40)
-    .attr("text-anchor", "middle")
-    .text("Food Consumption (kg/person/year)")
+  //   var xAxis = d3.axisBottom(xScale);
+  //   var yAxis = d3.axisLeft(yScale);
 
-  svg.append("text")
-    .attr("x", -height / 2)
-    .attr("y", padding - 40)
-    .attr("transform", "rotate(-90)")
-    .attr("text-anchor", "middle")
-    .text("CO2 Emissions (Kg CO2/person/year)")
+  //   svg.append("g")
+  //     .attr("transform", "translate(0, " + (height - padding) + ")")
+  //     .call(xAxis);
 
-  svg.selectAll("circle")
-    .data(formatted_data)
-    .join("circle")
-    .attr("cx", d => xScale(d.consumption))
-    .attr("cy", function (d) { return yScale(d.co2); })
-    .attr("r", 5)
-    .attr("opacity", 0.4)
-    .style("fill", function (d) { return colorScale(d.category); });
+  //   svg.append("g")
+  //     .attr("transform", "translate(" + padding + ", 0)")
+  //     .call(yAxis);
 
-  console.log(colorScale("Beef"))
-})
+  //   svg.append("text")
+  //     .attr("x", width / 2)
+  //     .attr("y", height - padding + 40)
+  //     .attr("text-anchor", "middle")
+  //     .text("Food Consumption (kg/person/year)")
+
+  //   svg.append("text")
+  //     .attr("x", -height / 2)
+  //     .attr("y", padding - 40)
+  //     .attr("transform", "rotate(-90)")
+  //     .attr("text-anchor", "middle")
+  //     .text("CO2 Emissions (Kg CO2/person/year)")
+
+  //   svg.selectAll("circle")
+  //     .data(formatted_data)
+  //     .join("circle")
+  //     .attr("cx", d => xScale(d.consumption))
+  //     .attr("cy", function (d) { return yScale(d.co2); })
+  //     .attr("r", 5)
+  //     .attr("opacity", 0.4)
+  //     .style("fill", function (d) { return colorScale(d.category); });
+
+  //   console.log(colorScale("Beef"))
+  // })
+
+});
+
