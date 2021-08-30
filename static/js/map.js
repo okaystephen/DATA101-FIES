1d388da467a5d082b2661a7db79a22cf9cffa29f
iravillanuevaa <ira_villanueva@dlsu.edu.ph>
2021-08-30 23:00:15 +0800

    added mapbox

---
+++ static/js/map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = '';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/iravillanueva/cksx86oy5c8s217rwkevw6ydl',
+    minZoom: 4,
+    maxZoom: 5.66,
+});
