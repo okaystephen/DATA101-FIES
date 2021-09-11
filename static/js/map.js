d586c94d3116a487d6634469c1e173d1a93376f4
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2021-09-11 16:13:40 +0800

    Tes map.js again if handlebar work

--- static/js/map.js
+++ static/js/map.js
@@ -1,11 +1,9 @@
-// mapboxgl.accessToken = {{mapbox_token}};
-
+mapboxgl.accessToken = "{{mapbox_token}}";
 var map = new mapboxgl.Map({
     container: 'map',
-    // style: 'mapbox://styles/iravillanueva/cksx86oy5c8s217rwkevw6ydl',
-    style: 'mapbox://styles/mujisteph/ckrx14qoi1qt917t64gi3439a',
-    center: [120.99, 14.56],
-    zoom: 12, // starting zoom,     
-    minZoom: 2,
+    style: 'mapbox://styles/iravillanueva/cksx86oy5c8s217rwkevw6ydl',
+    center: [122.20, 12.38],
+    zoom: 5, // starting zoom,     
+    minZoom: 5,
     maxZoom: 22
 });
