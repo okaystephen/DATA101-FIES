2ce158a7227aec233c7baab823862d96d0171fee
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2021-09-11 16:40:56 +0800

    Fixed /fies route

--- app.py
+++ app.py
@@ -60,9 +60,28 @@ def get_data():
 @app.route('/fies')
 def get_fies():
     df = pd.read_csv(data_url)
+    
     df = df.iloc[:, [0,1,2,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,33,34,35,47,50,51,54,55,56]] 
+    
+    df.replace({'Region' : {'I - Ilocos Region': 'Region I', 'II - Cagayan Valley': 'Region II', 
+                'III - Central Luzon' : 'Region III', 'IVA - CALABARZON': 'Region IVA',
+                'IVB - MIMAROPA':'Region IVB','V - Bicol Region':'Region V', 
+                'VI - Western Visayas':'Region VI','VII - Central Visayas': 'Region VII', 
+                'VIII - Eastern Visayas': 'Region VIII', 'IX - Zasmboanga Peninsula': 'Region IX',
+                'X - Northern Mindanao': 'Region X', 'XI - Davao Region': 'Region XI',
+                'XII - SOCCSKSARGEN': 'Region XII', 'Caraga': 'Region XIII', ' ARMM': 'ARMM'}}, inplace=True)
+
+    df.rename(columns= {'Crop Farming and Gardening expenses': 'Crop Farming and Gardening Expenditure', 
+            'Housing and water Expenditure': 'Housing and Water Expenditure',
+            'Restaurant and hotels Expenditure': 'Restaurant and Hotel Expenditure',
+            'Total Fish and  marine products Expenditure': 'Total Fish and Marine Products Expenditure',
+            'Members with age less than 5 year old': 'Members with Age less than 5 years old',
+            'Members with age 5 - 17 years old': 'Members with Age 5-17 years old',
+            'Total Number of Family members': 'Total Number of Family Members'}, inplace=True)
+
     df_groupby = df.groupby(by=["Region"]).mean().reset_index()
     data_dict = df_groupby.to_dict(orient="records")
+    
     data_json = json.dumps(data_dict) 
 
     return Response(data_json, mimetype="application/json")
